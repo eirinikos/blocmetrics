2b63b7808d30d611d7af3507769ccfd8a451465a
Andrea <eirinikos@gmail.com>
2015-11-11 17:40:51 -0800

    complete user signup

--- spec/requests/users_signups_spec.rb
+++ spec/requests/users_signups_spec.rb
@@ -13,3 +13,17 @@ RSpec.describe "invalid signup information", type: :request do
     assert_template 'users/new'
   end
 end
+
+RSpec.describe "valid signup information", type: :request do
+  it "allows a new user to be created" do
+    get signup_path
+    before_count = User.count
+    post_via_redirect users_path, user: {  name:  "Avocado Jones",
+                              email: "avocado@jones.org",
+                              password:  "foobar",
+                              password_confirmation: "foobar" }
+    after_count = User.count
+    expect(after_count).to eq(1)
+    assert_template 'users/show'
+  end
+end
